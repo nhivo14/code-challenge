4bf6d79d28845de0b2ac78ae8047ddf16bba6f20
Nhi Vo <nhi.vo@abcdigital.io>
2025-07-20 08:34:56 +0700

    solve problem 3

---
+++ src/problem3/existing-code.tsx
@@ -0,0 +1,81 @@
+interface WalletBalance {
+    currency: string;
+    amount: number;
+  }
+  interface FormattedWalletBalance {
+    currency: string;
+    amount: number;
+    formatted: string;
+  }
+  
+  interface Props extends BoxProps {
+  
+  }
+  const WalletPage: React.FC<Props> = (props: Props) => { 
+    const { children, ...rest } = props;
+    const balances = useWalletBalances();
+    const prices = usePrices();
+      // this function should be in a separate file or outside of the component
+      const getPriority = (blockchain: string): number => { // blockchain is not defined in WalletBalance and should be "string"
+        switch (blockchain) {
+          case 'Osmosis':
+            return 100
+          case 'Ethereum':
+            return 50
+          case 'Arbitrum':
+            return 30
+          case 'Zilliqa':
+            return 20
+          case 'Neo':
+            return 20
+          default:
+            return -99
+        }
+      }
+  
+    const sortedBalances = useMemo(() => {
+      return balances.filter((balance: WalletBalance) => {
+            const balancePriority = getPriority(balance.blockchain); // WalletBalance is missing blockchain property + balancePriority is not used anywhere
+            if (lhsPriority > -99) { // lhsPriority is not defined 
+               if (balance.amount <= 0) {
+                 return true;
+               }
+            }
+            return false
+          }).sort((lhs: WalletBalance, rhs: WalletBalance) => {
+              const leftPriority = getPriority(lhs.blockchain);
+            const rightPriority = getPriority(rhs.blockchain);
+            if (leftPriority > rightPriority) {
+              return -1;
+            } else if (rightPriority > leftPriority) {
+              return 1;
+            }
+      });
+    }, [balances, prices]);
+  
+    const formattedBalances = sortedBalances.map((balance: WalletBalance) => { // formattedBalances is not used anywhere
+      return {
+        ...balance,
+        formatted: balance.amount.toFixed()
+      }
+    })
+  
+    const rows = sortedBalances.map((balance: FormattedWalletBalance, index: number) => { // Should be formattedWalletBalance here
+      const usdValue = prices[balance.currency] * balance.amount;
+      return (
+        <WalletRow 
+          className={classes.row}
+          key={index} // key should be unique and not index
+          amount={balance.amount}
+          usdValue={usdValue}
+          formattedAmount={balance.formatted}
+        />
+      )
+    })
+  
+    return (
+      <div {...rest}>
+        {rows}
+      </div>
+    )
+  }
